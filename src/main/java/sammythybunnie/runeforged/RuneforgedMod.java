f05bc3dfe90fbe65f766bbdd0f53195db7d245fa
Sammythybunnie67 <96631619+Sammythybunnie67@users.noreply.github.com>
2023-08-20 14:43:05 +0100

    api finally works

--- src/main/java/sammythybunnie/runeforged/RuneforgedMod.java
+++ src/main/java/sammythybunnie/runeforged/RuneforgedMod.java
@@ -1,29 +1,20 @@
 package sammythybunnie.runeforged;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.Minecraft;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sammythybunnie.runeforged.api.uuidAPI.PlayerUUIDRetreiver;
-import sammythybunnie.runeforged.api.uuidAPI.PlayerUsernameExporter;
-
-import java.io.File;
-
+import sammythybunnie.runeforged.api.uuidAPI.UUIDHelper;
 
 
 public class RuneforgedMod implements ModInitializer {
-
-    @SuppressWarnings("ReassignedVariable")
-    public static void ModInitialisation() {
-            PlayerUsernameExporter.init(); // Initialize PlayerUsernameExporter first
-            PlayerUUIDRetreiver.init(); // Initialize PlayerUUIDRetriever
-        }
 
     public static final String MOD_ID = "runeforged";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        UUIDHelper.init();
         LOGGER.info("runeforged initialized.");
+        LOGGER.info("uuids and usernames obtained sucsessfully");
     }
 }
